3f60014850b39a7af6ebc9b37cfcf4db28d874ec
antonio nakic (toptal) <antonio.nakic@toptal.com>
2017-09-12 19:08:06 +0200

    Adding list to string converter

---
+++ Converters/ListToStringConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Thingie.WPF.Converters
+{
+	public class ListToStringConverter : MarkupExtension, IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			StringBuilder sb = new StringBuilder();
+			if (value is IEnumerable ie)
+			{
+				foreach (object x in ie)
+					sb.Append($"{x}, ");
+			}
+
+			if (sb.Length > 0)
+				sb.Remove(sb.Length - 2, 2);
+
+			return sb.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
+		}
+	}
+}
