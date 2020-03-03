c59882149223d295d65a38baad9ce380ff5f7f98
Dinko Gregorić <dinko@querystorm.com>
2020-03-03 17:54:03 +0100

    Rename IContextMenuItem file. Update documentation.

--- Controls/ObjectExplorer/IContextMenuItem.cs
+++ Controls/ObjectExplorer/IContextMenuItem.cs
@@ -1,7 +1,7 @@
 ﻿namespace Thingie.WPF.Controls.ObjectExplorer
 {
     /// <summary>
-    /// An interface that every context menu item needs to implement.
+    /// An interface that every context menu item needs to implement in order to be displayed in the context menu.
     /// </summary>
     public interface IContextMenuItem
     {
