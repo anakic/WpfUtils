7d127ba0f9ec9219517fdfbb6d83ea7dd29fb80e
Dinko Gregorić <dinko@querystorm.com>
2020-03-03 10:12:51 +0100

    Add styling to separator. Add comments to custom commands.

--- Controls/ObjectExplorer/CustomControlsCommand.cs
+++ Controls/ObjectExplorer/CustomControlsCommand.cs
@@ -1,7 +1,9 @@
-﻿using System;
-
-namespace Thingie.WPF.Controls.ObjectExplorer
+﻿namespace Thingie.WPF.Controls.ObjectExplorer
 {
+    /// <summary>
+    /// An empty implementation of ICustomCommand. 
+    /// Used as base for generating Control(System.Windows.Controls) commands.
+    /// </summary>
     public class CustomControlsCommand : ICustomCommand
     {
     }
--- Controls/ObjectExplorer/ICustomCommand.cs
+++ Controls/ObjectExplorer/ICustomCommand.cs
@@ -1,5 +1,8 @@
 ﻿namespace Thingie.WPF.Controls.ObjectExplorer
 {
+    /// <summary>
+    /// An empty command interface that every custom command needs to implement.
+    /// </summary>
     public interface ICustomCommand
     {
     }
