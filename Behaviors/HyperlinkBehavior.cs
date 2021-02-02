ea539be142d775bcff7e6dda04e002cbb07f2cd1
Dinko Gregorić <dinko@querystorm.com>
2021-02-02 11:13:43 +0100

    Add hyperlink behavior to underline hyperlink only on MouseEnter.

--- Behaviors/HyperlinkBehavior.cs
+++ Behaviors/HyperlinkBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace Thingie.WPF.Behaviors
 {
@@ -23,6 +24,30 @@ namespace Thingie.WPF.Behaviors
             if ((bool)args.NewValue)
             {
                 ((Hyperlink)target).RequestNavigate += (s, e) => System.Diagnostics.Process.Start(e.Uri.ToString());
+            }
+        }
+
+        public static bool GetUnderlineOnlyOnMouseEnter(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(UnderlineOnlyOnMouseEnterProperty);
+        }
+
+        public static void SetUnderlineOnlyOnMouseEnter(DependencyObject obj, bool value)
+        {
+            obj.SetValue(UnderlineOnlyOnMouseEnterProperty, value);
+        }
+
+        public static readonly DependencyProperty UnderlineOnlyOnMouseEnterProperty =
+            DependencyProperty.RegisterAttached("UnderlineOnlyOnMouseEnter", typeof(bool), typeof(Hyperlink), new PropertyMetadata(false, new PropertyChangedCallback(UnderlineOnlyOnMouseEnterPropertySet)));
+
+        private static void UnderlineOnlyOnMouseEnterPropertySet(DependencyObject target, DependencyPropertyChangedEventArgs args)
+        {
+            if ((bool)args.NewValue)
+            {
+                var hyperLink = (Hyperlink)target;
+                hyperLink.TextDecorations = null;
+                hyperLink.MouseEnter += (s, e) => hyperLink.TextDecorations = TextDecorations.Underline;
+                hyperLink.MouseLeave += (s, e) => hyperLink.TextDecorations = null;
             }
         }
     }
