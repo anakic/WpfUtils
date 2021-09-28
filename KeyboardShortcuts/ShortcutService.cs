e05c23b5ef123e0a7b2ff723382eaf6572f1efaa
Antonio <antonio@querystorm.com>
2021-09-28 10:28:03 +0200

    ShortcutService:
    1. KeyGesture is now public (internal was causing issues with remoting
    across processes)
    2. Adding ability to hook from another process

--- KeyboardShortcuts/ShortcutService.cs
+++ KeyboardShortcuts/ShortcutService.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 using Thingie.WPF.Attributes;
 
 namespace Thingie.WPF.KeyboardShortcuts
@@ -39,11 +41,11 @@ namespace Thingie.WPF.KeyboardShortcuts
         public abstract void Execute();
 
         //sluzi samo za komunikaciju sa property gridom (da objedini dva property-a u jedan, tako da key i modifier mogu editirati u istom retku u gridu)
-        internal ShortcutGesture KeyGesture { get { return new ShortcutGesture(Key, ModifierKeys); } set { Key = value.Key; ModifierKeys = value.ModifierKeys; } }
+        public ShortcutGesture KeyGesture { get { return new ShortcutGesture(Key, ModifierKeys); } set { Key = value.Key; ModifierKeys = value.ModifierKeys; } }
     }
 
     [ProxyFactory(typeof(ShortcutServiceProxyFactory))]
-    public class ShortcutService : MarshalByRefObject
+    public class ShortcutService : MarshalByRefObject, IDisposable
     {
         // todo: memory leak? when should we call RemotingServices.Disconnect(this)?
         public override object InitializeLifetimeService() => null;
@@ -51,15 +53,27 @@ namespace Thingie.WPF.KeyboardShortcuts
         public List<ShortcutHandle> Shortcuts { get; set; }
 
         public ShortcutService()
-            : this((uint)AppDomain.GetCurrentThreadId())
-        {
-        }
-
-        public ShortcutService(uint mainThreadId)
         {
             Shortcuts = new List<ShortcutHandle>();
             _proc = HookCallback;
-            _hookID = SetWindowsHookEx(WH_KEYBOARD, _proc, IntPtr.Zero, mainThreadId);
+            _hookID = SetWindowsHookEx(WH_KEYBOARD, _proc, IntPtr.Zero, (uint)AppDomain.GetCurrentThreadId());
+        }
+
+
+        // When you don't want the ProcessId, use this overload and pass IntPtr.Zero for the second parameter
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+
+        /// <summary>
+        /// Global hook for another process
+        /// </summary>
+        /// <param name="p"></param>
+        public ShortcutService(Process p)
+        {
+            Shortcuts = new List<ShortcutHandle>();
+            _proc = HookCallback;
+            _hookID = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, GetModuleHandle(p.MainModule.ModuleName), 0);
         }
 
         public void AddShortcut(ShortcutHandle handle)
@@ -72,42 +86,27 @@ namespace Thingie.WPF.KeyboardShortcuts
             this.Shortcuts.Remove(handle);
         }
 
-        #region winapi stuff
-        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-        private readonly LowLevelKeyboardProc _proc;
-        private readonly IntPtr _hookID = IntPtr.Zero;
-
-        //Declare the mouse hook constant.
-        //For other hook types, you can obtain these values from Winuser.h in the Microsoft SDK.            
-        private const int WH_KEYBOARD = 2;
-        private const int HC_ACTION = 0;
-
-        public void ReleaseHook()
-        {
-            UnhookWindowsHookEx(_hookID);
-        }
-
         private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
             try
             {
                 if (nCode == HC_ACTION)
-				{
-					ShortcutHandle shortcutHandle = FindShortcut();
+                {
+                    ShortcutHandle shortcutHandle = FindShortcut();
 
-					if (shortcutHandle != null)
-					{
-						if (shortcutHandle.CanExecute())
-						{
-							BeforeExecuteShortcut(shortcutHandle);
-							shortcutHandle.Execute();
-						}
-						return (IntPtr)(int)-1;
-					}
-					else
-						return CallNextHookEx(_hookID, nCode, wParam, lParam);
-				}
-				else
+                    if (shortcutHandle != null)
+                    {
+                        if (shortcutHandle.CanExecute())
+                        {
+                            BeforeExecuteShortcut(shortcutHandle);
+                            shortcutHandle.Execute();
+                        }
+                        return (IntPtr)(int)-1;
+                    }
+                    else
+                        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+                }
+                else
                     return CallNextHookEx(_hookID, nCode, wParam, lParam);
             }
             catch (Exception ex)
@@ -117,16 +116,37 @@ namespace Thingie.WPF.KeyboardShortcuts
             }
         }
 
-		private ShortcutHandle FindShortcut()
-		{
-			return Shortcuts.FirstOrDefault(sh =>
-				sh.KeyGesture.Key != Key.None &&
-				Keyboard.IsKeyDown(sh.KeyGesture.Key) &&
-				Keyboard.Modifiers == sh.KeyGesture.ModifierKeys &&
-				sh.IsInScope());
-		}
+        private ShortcutHandle FindShortcut()
+        {
+            return Shortcuts.FirstOrDefault(sh =>
+                sh.KeyGesture.Key != Key.None &&
+                Keyboard.IsKeyDown(sh.KeyGesture.Key) &&
+                Keyboard.Modifiers == sh.KeyGesture.ModifierKeys &&
+                sh.IsInScope());
+        }
 
-		protected virtual void BeforeExecuteShortcut(ShortcutHandle shortcutHandle) { }
+        protected virtual void BeforeExecuteShortcut(ShortcutHandle shortcutHandle) { }
+
+        public void Dispose()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        ~ShortcutService()
+        {
+            Dispose();
+        }
+
+        #region winapi stuff
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        //Declare the mouse hook constant.
+        //For other hook types, you can obtain these values from Winuser.h in the Microsoft SDK.            
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_KEYBOARD = 2;
+        private const int HC_ACTION = 0;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
@@ -138,10 +158,8 @@ namespace Thingie.WPF.KeyboardShortcuts
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
-        ~ShortcutService()
-        {
-            ReleaseHook();
-        }
+        [DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
 
         #endregion
     }
